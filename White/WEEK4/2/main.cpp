a804111f39353824e8b11079675f0cde80e61772
LedoRomus <r.ledovskih@gmail.com>
2019-06-09 22:25:10 +0300

    v1

---
+++ White/WEEK4/2/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+struct Specialization{
+string value;
+
+    explicit Specialization(string new_specialization)
+    {
+        value=new_specialization;
+    }
+};
+
+struct Course{
+string value;
+
+    explicit Course(string new_course)
+    {
+        value=new_course;
+    }
+};
+
+struct Week{
+string value;
+
+    explicit Week(string new_week)
+    {
+        value=new_week;
+    }
+};
+
+
+struct LectureTitle {
+  string specialization;
+  string course;
+  string week;
+
+  LectureTitle(const Specialization& new_specialization,const Course& new_course,const Week& new_week)
+  {
+        specialization=new_specialization.value;
+        course=new_course.value;
+        week=new_week.value;
+  }
+};
+
+int main()
+{
+    LectureTitle title(Specialization("C++"),Course("White belt"),Week("4th"));
+
+    //LectureTitle title(Course("White belt"),Specialization("C++"),Week("4th"));
+
+    //LectureTitle title("C++", "White belt", "4th");
+
+    //LectureTitle title(string("C++"), string("White belt"), string("4th"));
+
+    //LectureTitle title = {"C++", "White belt", "4th"};
+
+    //LectureTitle title = {{"C++"}, {"White belt"}, {"4th"}};
+
+    return 0;
+}
