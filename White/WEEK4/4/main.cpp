dd62dfb25d5c3f6b4efe6b077c64e697676924e6
LedoRomus <r.ledovskih@gmail.com>
2019-06-15 09:37:55 +0300

    v1

--- White/WEEK4/4/main.cpp
+++ White/WEEK4/4/main.cpp
@@ -32,7 +32,7 @@ public:
 			return source_value - value;
 		else if (operation == '*')
 			return source_value * value;
-		else 
+		else
 			return source_value / value;
 	}
 
@@ -86,14 +86,14 @@ Function MakeWeightFunction(const Params& params,
 	return function;
 }
 
-double ComputeImageWeight(const Params& params, const Image& image) {
+double ComputeImageWeight(const Params& params, const Image& image)
+{
 	Function function = MakeWeightFunction(params, image);
 	return function.Apply(image.quality);
 }
 
-double ComputeQualityByWeight(const Params& params,
-	const Image& image,
-	double weight) {
+double ComputeQualityByWeight(const Params& params,const Image& image,double weight)
+{
 	Function function = MakeWeightFunction(params, image);
 	function.Invert();
 	return function.Apply(weight);
